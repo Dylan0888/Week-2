6908092eb8bdaff851287d0a21332a421b1b51b3
Dylan0888 <Dylanj55521@outlook.com>
2022-01-25 16:51:04 +0000

    added new thangs

---
+++ Week2.py
@@ -0,0 +1,11 @@
+name = ["Lauren", "Dij", "Luke"]
+print (name[0] + " I love you!")
+
+place = ["Merica", "Egypt", "Skegness", "Poland", "Spain"]
+print (place)
+
+place.sort()
+print(place)
+
+place.sort(reverse=True)
+print (place)
